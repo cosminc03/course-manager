c25c9375043055bdb9093a617bc99c9909088303
astonished12 <dan_cehan@yahoo.com>
2017-01-17 14:00:59 +0200

    fix

---
+++ CourseManager.Web/Models/CourseViewModels/CourseCreateViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CourseManager.Web.Models.CourseViewModels
+{
+    public class CourseCreateViewModel
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public int Year { get; set; }
+        [Required]
+        public int Semester { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public Guid Id { get; set; }
+    }
+}
